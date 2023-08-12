f5d228b8810ae5333fad1b0360aaab774a6e5b63
João Pedro <joao.pedro@dtidigital.com.br>
2023-08-12 18:46:32 -0300

    clear the inputs when submiting the form

--- src/pages/Finances/components/ExpenseForm/view/ExpenseForm.tsx
+++ src/pages/Finances/components/ExpenseForm/view/ExpenseForm.tsx
@@ -2,58 +2,93 @@ import { useState, type ReactElement } from "react";
 import { Container } from "./styles";
 
 function ExpenseForm(): ReactElement {
-  /* 
-      1- First way to manipulate the user input
+  // 1- First way to manipulate the user input
   const [inputTitle, setInputTitle] = useState("");
   const [inputDate, setInputDate] = useState("");
   const [inputAmout, setInputAmout] = useState("");
-*/
 
   /*
       2 - Second way to manipulate the user input
 */
-  const [userInput, setUserInput] = useState({
+  /* 
+const [userInput, setUserInput] = useState({
     enteredTitle: "",
     enteredAmout: "",
     enteredDate: "",
   });
+  */
   const titleChangeHandler = (event: { target: { value: any } }) => {
-    //setInputTitle(event.target.value);
+    setInputTitle(event.target.value);
+
+    /* It will work but it's wrong for working with a lot of data
     setUserInput({
       ...userInput,
       enteredTitle: event.target.value,
     });
+    */
+    //If your state update depends on the previous state, it's necessary to pass em arrow function inside the state to unsure that the data is passing correctly
+    /*setUserInput((prevState) => {
+      return {...prevState, enteredTitle: event.target.value}
+    })
+    */
   };
   const amoutChangeHandler = (event: { target: { value: any } }) => {
-    //setInputAmout(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmout: event.target.value,
-    });
+    setInputAmout(event.target.value);
+
+    /* setUserInput((prevState) => {
+      return {...prevState, enteredAmout: event.target.value}
+    })
+    */
   };
   const dateChangeHandler = (event: { target: { value: any } }) => {
-    //setInputDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    setInputDate(event.target.value);
+
+    /*
+    setUserInput((prevState) => {
+      return {...prevState, enteredDate: event.target.value}
+    })
+    */
   };
+
+  const submitHandler = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: inputTitle,
+      amout: inputAmout,
+      date: new Date(inputDate),
+    };
+    console.log(expenseData);
+    setInputTitle("");
+    setInputDate("");
+    setInputAmout("");
+  };
+
   return (
     <Container>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="newExpenseControls">
           <div className="newExpenseControl">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler} />
+            <input
+              type="text"
+              value={inputTitle}
+              onChange={titleChangeHandler}
+            />
             <label>Date</label>
             <input
               type="date"
               min="2023-01-01"
               max="2025-12-31"
+              value={inputDate}
               onChange={dateChangeHandler}
             />
             <label>Amount</label>
-            <input type="number" onChange={amoutChangeHandler} />
+            <input
+              type="number"
+              value={inputAmout}
+              onChange={amoutChangeHandler}
+            />
           </div>
         </div>
         <div className="newExpenseActions">
